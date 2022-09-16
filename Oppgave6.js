efc27e249b8a76a616291901b7477f8d738de823
Christoffer <95276246+Midori04@users.noreply.github.com>
2022-09-16 13:33:40 +0200

    Update Oppgave6.js

--- Oppgave6.js
+++ Oppgave6.js
@@ -1,5 +1,5 @@
 const string = "0123456789";
 console.log(string);
-console.log(string.slice(0, 3)); // "01"
-console.log(string.slice(3, 6)); // "01234567"
-console.log(string.slice(6, 9)); // "3456"
+console.log(string.slice(0, 3));
+console.log(string.slice(3, 6)); 
+console.log(string.slice(6, 9)); 
